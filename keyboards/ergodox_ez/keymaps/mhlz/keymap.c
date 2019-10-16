e0d2b04dc538e681d476546f295879adcbd7bb9d
Mischa Holz <me@mhlz.de>
2019-10-16 13:48:26 +0200

    :fire: Remove layer toggles

--- keyboards/ergodox_ez/keymaps/mhlz/keymap.c
+++ keyboards/ergodox_ez/keymaps/mhlz/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
       // left hand
       KC_ESCAPE,      KC_1,        KC_2,        KC_3,        KC_4,        KC_5,          KC_6,
-      LT(PUNC, KC_TAB),         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,             KC_TRANSPARENT,
+      KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,             KC_TRANSPARENT,
       KC_LCTL,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
       KC_LSFT,        KC_Z,           KC_X,           KC_C,           LT(PUNC, KC_V),           KC_B,             MO(PUNC),
       KC_HYPR, HYPR(KC_P), KC_RALT, KC_LALT,        KC_LGUI,
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       // right hand
       KC_6,        KC_7,        KC_8,        KC_9,          KC_0,        KC_MINUS,     KC_EQL,
-      KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,             KC_O,           KC_P,               LT(PUNC, KC_BSLASH),
+      KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,             KC_O,           KC_P,               KC_BSLASH,
                       KC_H,           KC_J,           KC_K,             KC_L,           KC_SCOLON,          LCTL_T(KC_QUOTE),
       MO(PUNC),       KC_N,           LT(PUNC, KC_M),           KC_COMMA,         KC_DOT,         KC_SLASH,           KC_RSFT,
                                       KC_LGUI,        KC_LALT,   KC_TRANSPARENT, KC_TRANSPARENT,     MO(NUMB),

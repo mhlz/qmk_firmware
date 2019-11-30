f07b30a91670747be828691b4934c3b3eda0e974
Mischa Holz <me@mhlz.de>
2019-11-30 12:43:40 +0100

    Reorganize and reformat keymap

--- keyboards/ergodox_ez/keymaps/mhlz/keymap.c
+++ keyboards/ergodox_ez/keymaps/mhlz/keymap.c
@@ -1,9 +1,4 @@
 #include "ergodox_ez.h"
-#include "debug.h"
-#include "action_layer.h"
-#include "version.h"
-#include "keymap_german.h"
-#include "keymap_nordic.h"
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -17,166 +12,144 @@ enum custom_keycodes {
 #define GAME 1
 #define PUNC 2
 #define NUMB 3
-#define RSET 4
+#define CNFG 4
+
+#define KC_VPNC LT(PUNC, KC_V)
+#define KC_MPNC LT(PUNC, KC_M)
+#define KC_MOPC MO(PUNC)
+#define KC_MONU MO(NUMB)
+#define KC_HYP  HYPR(KC_P)
+#define KC_TOBA TO(BASE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
       // left hand
-      KC_ESCAPE,      KC_1,        KC_2,        KC_3,        KC_4,        KC_5,          KC_NO,
-      KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,             KC_TRANSPARENT,
-      KC_LCTL,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,
-      KC_LSFT,        KC_Z,           KC_X,           KC_C,           LT(PUNC, KC_V),           KC_B,             MO(PUNC),
-      KC_HYPR, HYPR(KC_P), KC_RALT, KC_LALT,        KC_LGUI,
+      KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_NO,
+      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_NO,
+      KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
+      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_VPNC,  KC_B,     KC_MOPC,
+      KC_HYPR,  KC_HYP,   KC_RALT,  KC_LALT,  KC_LGUI,
 
-                                                KC_MEDIA_PLAY_PAUSE,  KC_AUDIO_MUTE,
-                                                                      KC_F14,
-                                              KC_SPACE,   KC_BSPACE,  MO(PUNC),
+                                              KC_MPLY,  KC_MUTE,
+                                                                  KC_F14,
+                                              KC_SPC,   KC_BSPC,  KC_MOPC,
 
 
 
       // right hand
-      KC_NO,        KC_6,        KC_7,        KC_8,          KC_9,        KC_0,     KC_DLR,
-      KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,             KC_O,           KC_P,               KC_BSLASH,
-                      KC_H,           KC_J,           KC_K,             KC_L,           KC_SCOLON,          LCTL_T(KC_QUOTE),
-      MO(PUNC),       KC_N,           LT(PUNC, KC_M),           KC_COMMA,         KC_DOT,         KC_SLASH,           KC_RSFT,
-                                      KC_LGUI,        KC_LALT,   KC_TRANSPARENT, KC_TRANSPARENT,     MO(NUMB),
+      KC_NO,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DLR,
+      KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
+                KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOTE,
+      KC_MOPC,  KC_N,     KC_MPNC,  KC_COMM,  KC_DOT,   KC_SLASH, KC_RSFT,
+                          KC_LGUI,  KC_LALT,  KC_NO,    KC_NO,    KC_MONU,
 
-                                                KC_AUDIO_VOL_DOWN,    KC_AUDIO_VOL_UP,
-                                                KC_F15,
-                                                MO(PUNC),            KC_DELETE,     KC_ENTER
+                                              KC_VOLD,  KC_VOLU,
+                                              KC_F15,
+                                              KC_MOPC,  KC_DEL,   KC_ENTER
+  ),
+  [GAME] = LAYOUT_ergodox(
+      // left hand
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  KC_V,     _______,  _______,
+      _______,  _______,  _______,  _______,  _______,
+
+                                              _______,  _______,
+                                                                  _______,
+                                              _______,  _______,  _______,
+
+
+
+      // right hand
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  KC_M,     _______,  _______,  _______,  _______,
+                          _______,  _______,  _______,  _______,  _______,
+
+                                              _______,  _______,
+                                              _______,
+                                              _______,  _______,  _______
   ),
   [PUNC] = LAYOUT_ergodox(
       // left hand
-      TO(BASE),       KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,            KC_NO,
-      KC_TRANSPARENT, KC_NO,          KC_LCBR,        KC_RCBR,        KC_PLUS,        KC_ASTR,          KC_TRNS,
-      KC_TRANSPARENT, KC_AT,          KC_EQL,         KC_LPRN,        KC_RPRN,        KC_EXLM,
-      KC_TRANSPARENT, KC_NO,          KC_CIRC,        KC_PERC,        KC_UNDS,        KC_NO,            KC_TRNS,
-      KC_TRANSPARENT, KC_TRNS, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+      KC_TOBA,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_NO,
+      _______,  KC_NO,    KC_LCBR,  KC_RCBR,  KC_PLUS,  KC_ASTR,  _______,
+      _______,  KC_AT,    KC_EQL,   KC_LPRN,  KC_RPRN,  KC_EXLM,
+      _______,  KC_NO,    KC_CIRC,  KC_PERC,  KC_UNDS,  KC_NO,    _______,
+      _______,  _______,  _______,  _______,  _______,
 
-                                                KC_TRANSPARENT,   KC_TRANSPARENT,
-                                                                  KC_TRANSPARENT,
-                                KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT,
+                                              _______,  _______,
+                                                                  _______,
+                                              _______,  _______,  _______,
 
 
 
       // right hand
-      KC_NO,          KC_F6,          KC_F7,          KC_F8,            KC_F9,         KC_F10,             KC_F11,
-      KC_TRANSPARENT, KC_HASH,        KC_GRV,         KC_LBRC,          KC_RBRC,        KC_TILD,            KC_TRANSPARENT,
-                      KC_LEFT,        KC_DOWN,        KC_UP,            KC_RIGHT,       KC_DLR,             KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_NO,          KC_MINUS,       KC_AMPR,          KC_NO,          KC_NO,              KC_TRANSPARENT,
+      KC_NO,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
+      KC_F12,   KC_HASH,  KC_GRV,   KC_LBRC,  KC_RBRC,  KC_TILD,  _______,
+                KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_DLR,   _______,
+      _______,  KC_NO,    KC_MINUS, KC_AMPR,  KC_NO,    KC_NO,    _______,
+                          _______,  _______,  _______,  _______,  _______,
 
-                                      KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT, KC_TRANSPARENT,     KC_TRANSPARENT,
-
-                                                KC_TRANSPARENT,    TO(RSET),
-                                                KC_TRANSPARENT,
-                                                KC_TRANSPARENT,            KC_TRANSPARENT,     KC_TRANSPARENT
+                                              _______,  TO(CNFG),
+                                              _______,
+                                              _______,  _______,  _______
   ),
   [NUMB] = LAYOUT_ergodox(
       // left hand
-      TO(BASE),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_TRANSPARENT,   KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_KP_0,        KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_TRANSPARENT,   KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+      KC_TOBA,  _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  KC_KP_7,  KC_KP_8,  KC_KP_9,  _______,  _______,
+      _______,  KC_KP_0,  KC_KP_4,  KC_KP_5,  KC_KP_6,  _______,
+      _______,  _______,  KC_KP_1,  KC_KP_2,  KC_KP_3,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,
 
-                                                KC_TRANSPARENT,   KC_TRANSPARENT,
-                                                                  KC_TRANSPARENT,
-                                KC_KP_0, KC_TRANSPARENT,   KC_TRANSPARENT,
+                                              _______,  _______,
+                                                                  _______,
+                                              _______,  _______,  _______,
 
 
 
       // right hand
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                          _______,  _______,  _______,  _______,  _______,
 
-                                                KC_TRANSPARENT,    KC_TRANSPARENT,
-                                                KC_TRANSPARENT,
-                                                KC_TRANSPARENT,            KC_TRANSPARENT,     KC_TRANSPARENT
+                                              _______,  _______,
+                                              _______,
+                                              _______,  _______,  _______
   ),
+  [CNFG] = LAYOUT_ergodox(
+      // left hand
+      KC_TOBA,  TO(GAME), _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,
 
-  [RSET] = LAYOUT_ergodox(TO(BASE),KC_LSHIFT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RESET,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
+                                              _______,  _______,
+                                                                  _______,
+                                              _______,  _______,  _______,
 
+
+
+      // right hand
+      _______,  _______,  _______,  _______,  _______,  _______,  RESET,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                _______,  _______,  _______,  _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                          _______,  _______,  _______,  _______,  _______,
+
+                                              _______,  _______,
+                                              _______,
+                                              _______,  _______,  _______
+  )
 };
-
-const uint16_t PROGMEM fn_actions[] = {
-  [1] = ACTION_LAYER_TAP_TOGGLE(1)
-};
-
-// leaving this in place for compatibilty with old keymaps cloned and re-compiled.
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-      switch(id) {
-        case 0:
-        if (record->event.pressed) {
-          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        }
-        break;
-      }
-    return MACRO_NONE;
-};
-
-void matrix_init_user(void) {
-#ifdef RGBLIGHT_COLOR_LAYER_0
-  rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
-#endif
-};
-
-void matrix_scan_user(void) {
-  if (keyboard_report->mods & (MOD_BIT(KC_LSFT))) {
-    ergodox_right_led_3_on();
-  } else if (keyboard_report->mods & (MOD_BIT(KC_RSFT))) {
-    ergodox_right_led_3_on();
-  } else if (keyboard_report->mods & (MOD_BIT(KC_LCTRL))) {
-    ergodox_right_led_3_on();
-  } else if (keyboard_report->mods & (MOD_BIT(KC_RCTRL))) {
-    ergodox_right_led_3_on();
-  } else if (keyboard_report->mods & (MOD_BIT(KC_LALT))) {
-    ergodox_right_led_3_on();
-  } else if (keyboard_report->mods & (MOD_BIT(KC_RALT))) {
-    ergodox_right_led_3_on();
-  } else if (keyboard_report->mods & (MOD_BIT(KC_LGUI))) {
-    ergodox_right_led_3_on();
-  } else if (keyboard_report->mods & (MOD_BIT(KC_RGUI))) {
-    ergodox_right_led_3_on();
-  } else {
-    int layer = biton32(layer_state);
-    if (layer != RSET) {
-      ergodox_right_led_3_off();
-    }
-  }
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    // dynamically generate these.
-    case EPRM:
-      if (record->event.pressed) {
-        eeconfig_init();
-      }
-      return false;
-      break;
-    case VRSN:
-      if (record->event.pressed) {
-        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-      }
-      return false;
-      break;
-    case RGB_SLD:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-      }
-      return false;
-      break;
-
-  }
-  return true;
-}
 
 uint32_t layer_state_set_user(uint32_t state) {
-
     uint8_t layer = biton32(state);
 
     ergodox_board_led_off();
@@ -197,7 +170,7 @@ uint32_t layer_state_set_user(uint32_t state) {
         ergodox_right_led_1_on();
         ergodox_right_led_2_on();
         break;
-      case RSET:
+      case CNFG:
         ergodox_right_led_1_on();
         ergodox_right_led_2_on();
         ergodox_right_led_3_on();
@@ -205,5 +178,4 @@ uint32_t layer_state_set_user(uint32_t state) {
     }
 
     return state;
-
 };

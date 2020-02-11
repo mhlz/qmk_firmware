8faffc8d995817f0907691fc0ab3d047c6ef8464
Mischa Holz <me@mhlz.de>
2020-02-11 14:09:26 +0100

    Adjust spacing

--- keyboards/redox_w/keymaps/mhlz/keymap.c
+++ keyboards/redox_w/keymaps/mhlz/keymap.c
@@ -19,6 +19,8 @@ extern keymap_config_t keymap_config;
 #define KC_MONU MO(NUMB)
 #define KC_HYP  HYPR(KC_P)
 #define KC_TOBA TO(BASE)
+#define KC_SPGU MT(MOD_LGUI, KC_SPACE)
+#define KC_ENTG MT(MOD_LGUI, KC_ENTER)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -34,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_DLR  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_NO   ,                          KC_NO   ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS ,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_MOPC ,                          KC_MOPC ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOTE,
-  //├───────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_VPNC ,KC_B    ,KC_MPLY ,KC_MUTE ,        KC_VOLD ,KC_VOLU ,KC_N    ,KC_MPNC ,KC_COMM ,KC_DOT  ,KC_SLASH,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      KC_HYPR ,KC_HYP  ,KC_LALT ,KC_LGUI ,     KC_SPACE,    KC_BSPC ,KC_MOPC ,        KC_MOPC ,KC_DEL  ,    KC_ENTER,     KC_LGUI ,KC_LALT ,XXXXXXX ,KC_MONU

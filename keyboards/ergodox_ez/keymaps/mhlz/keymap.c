c51ba5c0e01861735b711fdea92ae2888a277334
Mischa Holz <me@mhlz.de>
2020-11-12 18:18:09 +0100

    Update ergodox keymap

--- keyboards/ergodox_ez/keymaps/mhlz/keymap.c
+++ keyboards/ergodox_ez/keymaps/mhlz/keymap.c
@@ -11,8 +11,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox(
       // left hand
-      KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_NO,
-      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_NO,
+      KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_VOLD,
+      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     C(KC_F11),
       KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
       KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,  KC_B,     KC_MOPC,
       KC_HYPR,  KC_HYP,   KC_RALT,  KC_LALT,  KC_LGUI,
@@ -24,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
       // right hand
-      KC_NO,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DLR,
-      KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
+      KC_VOLU,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DLR,
+      C(KC_F12),KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
                 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOTE,
       KC_MOPC,  KC_N,     KC_M,  KC_COMM,  KC_DOT,   KC_SLASH, KC_RSFT,
                           KC_LGUI,  KC_LALT,  KC_NO,    KC_NO,    KC_MONU,
